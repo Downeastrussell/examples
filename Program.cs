34efb7d4f9d3411238ed1447aaa669fe87252196
Russell Miller <45610992+Downeastrussell@users.noreply.github.com>
2020-01-27 12:22:18 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> students = new List<string>() {
+                "Megan", "Damon", "Chase", "Tekisha",
+                "Castle", "Mark", "Keith", "Adam",
+                "Patrick", "Hannah", "Mike"
+            };
+
+            // Can't do this easily with a base array
+            students.Add("Melanie");
+            students.Insert(3, "Simon");
+
+            if (students.Contains("Chase"))
+            {
+                Console.WriteLine("Must be cohort 13");
+            }
+
+            // This looks a lot like JavaScript!
+            students.ForEach(student => Console.WriteLine(student));
+        }
+    }
+}
